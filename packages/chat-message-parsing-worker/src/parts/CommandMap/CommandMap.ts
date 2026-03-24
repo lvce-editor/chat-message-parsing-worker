09fbb3724f814f23fba4c9732f3ab95a237e7779
Le Vivilet <levivilet@pm.me>
2026-03-24 13:10:14 +0100

    feature: add parseMessageContent to commandMap

--- packages/chat-message-parsing-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/chat-message-parsing-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,9 @@
 import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
+import { parseMessageContent } from '../ParseMessageContent/ParseMessageContent.ts'
 import * as NetworkCommandMap from './NetworkCommandMap.ts'
 
 export const commandMap = {
   ...NetworkCommandMap.networkCommandMap,
+  'ChatParser.parseMessageContent': parseMessageContent,
   'HandleMessagePort.handleMessagePort': handleMessagePort,
 }
