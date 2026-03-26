b3f90dec6b35d60487b90d088a66b078b8325065
Le Vivilet <levivilet@pm.me>
2026-03-26 14:39:19 +0100

    feature: remove unused network command map and related API request files (#6)

--- packages/chat-message-parsing-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/chat-message-parsing-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,9 +1,7 @@
 import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
 import { parseMessageContent } from '../ParseMessageContent/ParseMessageContent.ts'
-import * as NetworkCommandMap from './NetworkCommandMap.ts'
 
 export const commandMap = {
-  ...NetworkCommandMap.networkCommandMap,
   'ChatParser.parseMessageContent': parseMessageContent,
   'HandleMessagePort.handleMessagePort': handleMessagePort,
 }
