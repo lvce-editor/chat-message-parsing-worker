233d74f9bb8e4b0e0ed93a26ab41de558f1b583e
Le Vivilet <levivilet@pm.me>
2026-03-26 14:41:53 +0100

    feature: add parseMessageContents function and update commandMap

--- packages/chat-message-parsing-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/chat-message-parsing-worker/src/parts/CommandMap/CommandMap.ts
@@ -1,7 +1,9 @@
 import { handleMessagePort } from '../HandleMessagePort/HandleMessagePort.ts'
 import { parseMessageContent } from '../ParseMessageContent/ParseMessageContent.ts'
+import { parseMessageContents } from '../ParseMessageContents/ParseMessageContents.ts'
 
 export const commandMap = {
   'ChatParser.parseMessageContent': parseMessageContent,
+  'ChatParser.parseMessageContents': parseMessageContents,
   'HandleMessagePort.handleMessagePort': handleMessagePort,
 }
