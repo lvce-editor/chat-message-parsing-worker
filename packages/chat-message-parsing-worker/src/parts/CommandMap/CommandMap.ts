f30751fa216a31cb931c21c1f49d85be4fc02b8f
Le Vivilet <levivilet@pm.me>
2026-03-29 08:49:53 +0200

    feature: refactor commandMap and handleMessagePort to use PlainMessagePortRpc

--- packages/chat-message-parsing-worker/src/parts/CommandMap/CommandMap.ts
+++ packages/chat-message-parsing-worker/src/parts/CommandMap/CommandMap.ts
@@ -3,7 +3,10 @@ import { parseMessageContent } from '../ParseMessageContent/ParseMessageContent.
 import { parseMessageContents } from '../ParseMessageContents/ParseMessageContents.ts'
 
 export const commandMap = {
+  'ChatMessageParsing.parseMessageContent': parseMessageContent,
+  'ChatMessageParsing.parseMessageContents': parseMessageContents,
   'ChatParser.parseMessageContent': parseMessageContent,
   'ChatParser.parseMessageContents': parseMessageContents,
   'HandleMessagePort.handleMessagePort': handleMessagePort,
+  initialize: (_: string, port: MessagePort): Promise<void> => handleMessagePort(port),
 }
