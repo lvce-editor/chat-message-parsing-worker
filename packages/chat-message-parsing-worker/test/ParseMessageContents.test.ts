0c6c41e8d49814b4f75fdbe3178d80dd26957df0
Le Vivilet <levivilet@pm.me>
2026-03-29 17:10:56 +0200

    fix

--- packages/chat-message-parsing-worker/test/ParseMessageContents.test.ts
+++ packages/chat-message-parsing-worker/test/ParseMessageContents.test.ts
@@ -1,6 +1,17 @@
 import { expect, test } from '@jest/globals'
 import { parseMessageContents } from '../src/parts/ParseMessageContents/ParseMessageContents.ts'
 
+const orderedListItem = (text: string, index: number) => ({
+  children: [
+    {
+      text,
+      type: 'text' as const,
+    },
+  ],
+  index,
+  type: 'list-item' as const,
+})
+
 test('parseMessageContents should parse each message independently', () => {
   expect(parseMessageContents(['hello', '1. first\n2. second'])).toEqual([
     [
@@ -17,24 +28,8 @@ test('parseMessageContents should parse each message independently', () => {
     [
       {
         items: [
-          {
-            children: [
-              {
-                text: 'first',
-                type: 'text',
-              },
-            ],
-            type: 'list-item',
-          },
-          {
-            children: [
-              {
-                text: 'second',
-                type: 'text',
-              },
-            ],
-            type: 'list-item',
-          },
+          orderedListItem('first', 1),
+          orderedListItem('second', 2),
         ],
         type: 'ordered-list',
       },
