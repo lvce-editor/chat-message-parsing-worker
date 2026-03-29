1c2a021513cb22a5b187e450fca2157171def85c
Le Vivilet <levivilet@pm.me>
2026-03-29 17:01:28 +0200

    feature: implement chat math worker initialization in the listen function (#17)

--- packages/chat-message-parsing-worker/src/parts/Listen/Listen.ts
+++ packages/chat-message-parsing-worker/src/parts/Listen/Listen.ts
@@ -1,8 +1,10 @@
 import { WebWorkerRpcClient } from '@lvce-editor/rpc'
 import * as CommandMap from '../CommandMap/CommandMap.ts'
+import { initializeChatMathWorker } from '../InitializeChatMathWorker/InitializeChatMathWorker.ts'
 
 export const listen = async (): Promise<void> => {
   await WebWorkerRpcClient.create({
     commandMap: CommandMap.commandMap,
   })
+  await initializeChatMathWorker()
 }
